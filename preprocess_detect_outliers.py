22f5a9b99db21cca0525aebad52b8cd397ffdace
Unknown <roelbouman@gmail.com>
2020-05-06 21:55:52 +0200

    Added LMDD and LODA

--- preprocess_detect_outliers.py
+++ preprocess_detect_outliers.py
@@ -55,9 +55,17 @@ from pyod.models.abod import ABOD
 from pyod.models.cblof import CBLOF
 from pyod.models.cof import COF
 from pyod.models.hbos import HBOS
-from pyod.models.knn import KNN 
 from pyod.models.iforest import IForest
+from pyod.models.knn import KNN 
+from pyod.models.lmdd import LMDD
+from pyod.models.loda import LODA
 
+real_metrics = ["cosine", "euclidean", "manhattan", "braycurtis", "canberra", "chebyshev", "correlation", "dice", "hamming", "jaccard", "kulsinski", "mahalanobis", "matching", "rogerstanimoto", "russellrao", "seuclidean", "sokalmichener", "sokalsneath", "sqeuclidean", "yule"]
+test_metrics = ['cityblock', 'cosine', 'euclidean', 'l1', 'l2', 'manhattan', 'braycurtis', 'canberra', 'chebyshev',
+          'correlation', 'dice', 'hamming', 'jaccard', 'kulsinski',
+          'mahalanobis', 'matching', 'minkowski', 'rogerstanimoto',
+          'russellrao', 'seuclidean', 'sokalmichener', 'sokalsneath',
+          'sqeuclidean', 'yule']
 
 #define parameters for each method:
 
@@ -66,8 +74,10 @@ abod_parameters = {"method":["fast"]}
 cblof_parameters = {"n_clusters":[8,9], "alpha":[0.8,0.9], "beta":[4,5], "use_weights":[False, True]}
 cof_parameters = {"n_neighbors":[2,3]}
 hbos_parameters = {"n_bins":[10,20,30], "alpha":[0.1,0.2,0.3]}
-knn_parameters = {"n_neighbors":range(1,20)}
-iforest_parameters = {"max_features":[1,2,3,4,5], "bootstrap":[True, False]}
+iforest_parameters = {"n_estimators":[1000], "max_samples":[0.1,0.2,0.3,0.5,0.6,0.7,0.8,0.9,1.0],  "max_features":[0.1,0.2,0.3,0.5,0.6,0.7,0.8,0.9,1.0], "bootstrap":[True, False]}
+knn_parameters = {"n_neighbors":range(1,20), "method":["mean", "largest", "median"], "metric":test_metrics}
+lmdd_parameters = {"n_iter":[50,100,200], "dis_measure":["aad", "var", "iqr"]}
+loda_parameters = {"n_bins":[10,20,50,100,200], "random_cuts":[50,100,200,500]}
 
 #nested dict of methods and parameters
 methods_params = {
@@ -77,7 +87,9 @@ methods_params = {
         "COF":{"method":COF, "params":cof_parameters},
         "HBOS":{"method":HBOS, "params":hbos_parameters},
         "KNN":{"method":KNN, "params":knn_parameters},
-        "iforest":{"method":IForest, "params":iforest_parameters}
+        "iforest":{"method":IForest, "params":iforest_parameters},
+        "LMDD":{"method":LMDD, "params":lmdd_parameters},
+        "LODA":{"method":LODA, "params":loda_parameters},
         }
     
 
@@ -95,9 +107,9 @@ methods_params = {
 #         "cof":{"method":COF, "params":cof_parameters}
 #         }
 
-methods_params = {
-        "HBOS":{"method":HBOS, "params":hbos_parameters},
-}
+# methods_params = {
+#         "HBOS":{"method":HBOS, "params":hbos_parameters},
+# }
 
 # methods_params = {
 #         "iforest":{"method":IForest, "params":iforest_parameters}
@@ -106,6 +118,14 @@ methods_params = {
 # methods_params = {
 #         "KNN":{"method":KNN, "params":knn_parameters}
 #         }
+
+methods_params = {
+        "LMDD":{"method":LMDD, "params":lmdd_parameters},
+}
+
+methods_params = {
+        "LODA":{"method":LODA, "params":loda_parameters},
+}
 
 
 picklefile_names = os.listdir(pickle_dir)[2:4]
