f602dec00ac035a8f926a4d91e659371bbbb3ccd
Roel <Bouman>
2022-10-20 18:50:31 +0200

    Updated hyperparameters for neural methods and rgraph

--- preprocess_detect_outliers.py
+++ preprocess_detect_outliers.py
@@ -44,7 +44,7 @@ arg_parser = argparse.ArgumentParser(description='Run selected methods over all 
 arg_parser.add_argument('--method',
                        metavar='M',
                        dest='method',
-                       default='RGraph',
+                       default='all',
                        type=str,
                        help='The method that you would like to run')
 
@@ -82,7 +82,6 @@ from pyod.models.lof import LOF
 from pyod.models.mcd import MCD
 from pyod.models.ocsvm import OCSVM
 from pyod.models.pca import PCA
-from pyod.models.sod import SOD
 from pyod.models.ecod import ECOD
 #from pyod.models.sos import SOS #SOS also has memory allocation issues.
 from pyod.models.combination import maximization
@@ -90,12 +89,11 @@ from pyod.models.combination import maximization
 from additional_methods.ensemble import  Ensemble
 from additional_methods.wrappers.ExtendedIForest import ExtendedIForest
 from additional_methods.ODIN import ODIN
-from additional_methods.gen2out.gen2out import gen2Out
+#from additional_methods.gen2out.gen2out import gen2Out
 
 from additional_methods.wrappers.AE import AE_wrapper
 from additional_methods.wrappers.VAE import VAE_wrapper
 from additional_methods.wrappers.AnoGAN import AnoGAN_wrapper
-from additional_methods.wrappers.DeepSVDD import DeepSVDD_wrapper
 
 ensemble_LOF_krange = range(5,31)
 
@@ -108,7 +106,7 @@ method_classes = {
         "ABOD":ABOD, 
         "CBLOF":CBLOF,
         "u-CBLOF":CBLOF,
-        #"COF":COF,
+        "COF":COF,
         "COPOD":COPOD,
         "HBOS":HBOS,
         "kNN":KNN,
@@ -121,35 +119,26 @@ method_classes = {
         "MCD":MCD,
         "OCSVM":OCSVM,
         "PCA":PCA, 
-        #"SOD":SOD,
         "EIF":ExtendedIForest,
         "ODIN":ODIN,
         "ECOD":ECOD,
         # "gen2out":gen2Out()
-        "1-layer-AE":AE_wrapper,
-        "2-layer-AE":AE_wrapper,
-        "3-layer-AE":AE_wrapper,
-        "1-layer-VAE":VAE_wrapper,
-        "2-layer-VAE":VAE_wrapper,
-        "3-layer-VAE":VAE_wrapper,
-        "1-layer-beta-VAE":VAE_wrapper,
-        "2-layer-beta-VAE":VAE_wrapper,
-        "3-layer-beta-VAE":VAE_wrapper,
-        #"2-layer-AnoGAN":AnoGAN_wrapper,
-        #"DeepSVDD":DeepSVDD_wrapper,
-        #"AE-DeepSVDD":DeepSVDD_wrapper
+        "AE":AE_wrapper,
+        "VAE":VAE_wrapper,
+        "beta-VAE":VAE_wrapper,
+        "AnoGAN":AnoGAN_wrapper
         }
 
 #dict of methods and parameters
 method_parameters = {
-        "RGraph":{"gamma":[5,50,200,350,500]},
+        "RGraph":{"gamma":[5,50,200,350,500], "algorithm":["lasso_cd"]}, #use lasso_cd due to convergence issues
         "INNE":{},
         "GMM":{"n_components":range(2,15)},
         "KDE":{},
         "ABOD":{"method":["fast"], "n_neighbors":[60]}, 
         "CBLOF":{"n_clusters":range(2,15), "alpha":[0.7,0.8,0.9], "beta":[3,5,7], "use_weights":[True]},
         "u-CBLOF":{"n_clusters":range(2,15), "alpha":[0.7,0.8,0.9], "beta":[3,5,7], "use_weights":[False]},
-        #"COF":{"n_neighbors":[5,10,15,20,25,30]},
+        "COF":{"n_neighbors":[5,10,15,20,25,30]},
         "COPOD":{},
         "HBOS":{"n_bins":["auto"]},
         "kNN":{"n_neighbors":range(5,31), "method":["mean"]},
@@ -167,18 +156,11 @@ method_parameters = {
         "ODIN":{"n_neighbors":range(5,31)},
         "ECOD":{},
         # "gen2out":
-        "1-layer-AE":{"n_layers":[1], "shrinkage_factor":[0.3,0.5,0.7,0.9], "verbose":[0]},
-        "2-layer-AE":{"n_layers":[2], "shrinkage_factor":[0.3,0.5], "verbose":[0]},
-        "3-layer-AE":{"n_layers":[3], "shrinkage_factor":[0.3,0.5], "verbose":[0]},
-        "1-layer-VAE":{"n_layers":[1], "shrinkage_factor":[0.3,0.5,0.7,0.9], "verbose":[0]},
-        "2-layer-VAE":{"n_layers":[2], "shrinkage_factor":[0.3,0.5], "verbose":[0]},
-        "3-layer-VAE":{"n_layers":[3], "shrinkage_factor":[0.3,0.5], "verbose":[0]},
-        "1-layer-beta-VAE":{"n_layers":[1], "shrinkage_factor":[0.3,0.5,0.7,0.9], "gamma":[10,20,50], "verbose":[0]},
-        "2-layer-beta-VAE":{"n_layers":[2], "shrinkage_factor":[0.3,0.5], "gamma":[10,20,50], "verbose":[0]},
-        "3-layer-beta-VAE":{"n_layers":[3], "shrinkage_factor":[0.3,0.5], "gamma":[10,20,50], "verbose":[0]},
-        "2-layer-AnoGAN":{"D_n_layers":[2], "D_shrinkage_factor":[0.3,0.5], "G_n_layers":[2], "G_shrinkage_factor":[0.3,0.5],  "verbose":[0], "epochs":[50]},
-        #"DeepSVDD":{"n_layers":[2], "shrinkage_factor":[0.3,0.5], "verbose":[0]},
-        #"AE-DeepSVDD":{"n_layers":[2], "shrinkage_factor":[0.3,0.5], "verbose":[0], "use_ae":[True]}
+        "AE":{"n_layers":[1,2,3], "shrinkage_factor":[0.2,0.3,0.5], "dropout_rate":[0], "epochs":[200], "validation_size":[0.2], "output_activation":["linear"], "verbose":[0]},
+        "VAE":{"n_layers":[1,2,3], "shrinkage_factor":[0.2,0.3,0.5], "dropout_rate":[0], "epochs":[200], "validation_size":[0.2], "output_activation":["linear"], "verbose":[0]},
+        "beta-VAE":{"n_layers":[1,2,3], "shrinkage_factor":[0.2,0.3,0.5], "dropout_rate":[0], "epochs":[200], "validation_size":[0.2], "output_activation":["linear"], "gamma":[10,20,50], "verbose":[0]},
+        "AnoGAN":{"D_n_layers":[3], "D_shrinkage_factor":[0.3,0.5], "G_n_layers":[3], "G_shrinkage_factor":[0.3,0.5],  "verbose":[0], "epochs":[200]},
+
         }
 
 #%% 
@@ -290,7 +272,7 @@ for picklefile_name in picklefile_names:
                 np.savetxt(target_scorefile_name, outlier_scores)
                 
                 #write Keras history
-                if method_name in ["VAE", "beta-VAE", "AE", "AnoGAN", "DeepSVDD"]:
+                if method_name in ["VAE", "beta-VAE", "AE", "AnoGAN"]:
                     if method_name == "AnoGAN":
                         history_df = pd.DataFrame({"discriminator_loss":pipeline[1].hist_loss_discriminator, "generator_loss":pipeline[1].hist_loss_generator})
                     else:
