23bee56075f8b0e20a696b0dfec1a9491770c6ec
Roel Bouman <r.bouman@science.ru.nl>
2022-12-01 19:42:23 +0100

    Changed name of anomaly type generation script

--- generate_and_plot_types_of_anomalies.py
+++ generate_and_plot_types_of_anomalies.py
@@ -3,10 +3,8 @@ import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-sns.set_style("whitegrid")
+sns.set_style("white")
 
-#%%
-plt.figure()
 
 #%%
 
@@ -14,11 +12,11 @@ plt.figure()
 
 distribution_1 = np.random.randn(1000,2) + np.array([1,1])
 
-outliers_1 = np.array([[-3,5], [1, -3.5], [-4,-3]], dtype=np.float32)
+anomalies_1 = np.array([[-3,5], [1, -3.5], [-4,-3]], dtype=np.float32)
 
 
 sns.scatterplot(x=distribution_1[:,0], y=distribution_1[:,1], label="Cluster 1", color="blue")
-sns.scatterplot(x=outliers_1[:,0], y=outliers_1[:,1], marker="X", label="Outlier", color="red")
+sns.scatterplot(x=anomalies_1[:,0], y=anomalies_1[:,1], marker="X", label="Outlier", color="red")
 plt.xlim(-6, 10)
 plt.ylim(-6, 10)
 plt.legend(loc='upper left')
@@ -34,15 +32,15 @@ plt.figure()
 
 distribution_2 = np.random.randn(500,2)/5 + np.array([7,7])
 
-outliers_2 = np.array([[6.2,6.5], [7.2, 8], [7.9,6.3]])
+anomalies_2 = np.array([[6.2,6.5], [7.2, 8], [7.9,6.3]])
 
-outliers = np.concatenate((outliers_1, outliers_2))
+anomalies = np.concatenate((anomalies_1, anomalies_2))
 
 plt.figure()
 sns.scatterplot(x=distribution_1[:,0], y=distribution_1[:,1], label="Cluster 1", color="blue")
 sns.scatterplot(x=distribution_2[:,0], y=distribution_2[:,1], label="Cluster 2", color="green")
 
-sns.scatterplot(x=outliers[:,0], y=outliers[:,1], marker="X", label="Outlier", color="red")
+sns.scatterplot(x=anomalies[:,0], y=anomalies[:,1], marker="X", label="Outlier", color="red")
 plt.xlim(-6, 10)
 plt.ylim(-6, 10)
 plt.legend(loc='upper left')
