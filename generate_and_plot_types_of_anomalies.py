663f1791cdab78af888374f417ad83670fea00a6
Roel Bouman <r.bouman@science.ru.nl>
2024-01-12 11:42:48 +0100

    Updated anomaly examples to now do 4 subplots rather than 8.

--- generate_and_plot_types_of_anomalies.py
+++ generate_and_plot_types_of_anomalies.py
@@ -8,188 +8,160 @@ sns.set_style("white")
 
 #%% peripheral point
 
-peripheral_point_plot = plt.figure()
+# Peripheral point plot
+distribution_1 = np.random.randn(1000, 2) + np.array([1, 1])
+anomalies_1 = np.array([[-3, 5], [1, -3.5], [-4, -3]], dtype=np.float32)
 
-distribution_1 = np.random.randn(1000,2) + np.array([1,1])
+fig, axes = plt.subplots(1, 2, figsize=(18, 6))
 
-anomalies_1 = np.array([[-3,5], [1, -3.5], [-4,-3]], dtype=np.float32)
+sns.scatterplot(x=distribution_1[:, 0], y=distribution_1[:, 1], color="blue", alpha=0.2, ax=axes[0])
+sns.scatterplot(x=anomalies_1[:, 0], y=anomalies_1[:, 1], marker="X", color="red", s=100, ax=axes[0])
+axes[0].set_xlim(-6, 10)
+axes[0].set_ylim(-6, 10)
+axes[0].set_xlabel("X$_1$")
+axes[0].set_ylabel("X$_2$")
+axes[0].set_title("Peripheral Anomalies", fontsize=20)
 
+# Enclosed point plot
+distribution_1 = np.random.randn(1000, 2) + np.array([-4, -4])
+distribution_2 = np.random.randn(1000, 2) + np.array([-4, 4])
+distribution_3 = np.random.randn(1000, 2) + np.array([4, -4])
+distribution_4 = np.random.randn(1000, 2) + np.array([4, 4])
 
-sns.scatterplot(x=distribution_1[:,0], y=distribution_1[:,1], color="blue", alpha=0.2)
-sns.scatterplot(x=anomalies_1[:,0], y=anomalies_1[:,1], marker="X", color="red", s=100)
-plt.xlim(-6, 10)
-plt.ylim(-6, 10)
+anomalies_1 = np.array([[0, 0], [-0.5, 0.3], [0.4, -0.7]], dtype=np.float32)
 
-plt.xlabel("X$_1$")
-plt.ylabel("X$_2$")
+sns.scatterplot(x=distribution_1[:, 0], y=distribution_1[:, 1], color="blue", alpha=0.2, ax=axes[1])
+sns.scatterplot(x=distribution_2[:, 0], y=distribution_2[:, 1], color="blue", alpha=0.2, ax=axes[1])
+sns.scatterplot(x=distribution_3[:, 0], y=distribution_3[:, 1], color="blue", alpha=0.2, ax=axes[1])
+sns.scatterplot(x=distribution_4[:, 0], y=distribution_4[:, 1], color="blue", alpha=0.2, ax=axes[1])
+sns.scatterplot(x=anomalies_1[:, 0], y=anomalies_1[:, 1], marker="X", color="red", s=100, ax=axes[1])
+axes[1].set_xlim(-8, 8)
+axes[1].set_ylim(-8, 8)
+axes[1].set_xlabel("X$_1$")
+axes[1].set_ylabel("X$_2$")
+axes[1].set_title("Enclosed Anomalies", fontsize=20)
 
-#plt.title("Peripheral Anomalies")
+# Adjust layout to prevent clipping of titles
+plt.tight_layout()
 
-plt.savefig("figures/peripheral_point_example.pdf", format="pdf")
-
-#%% enclosed point plot
-
-
-enclosed_point_plot = plt.figure()
-
-distribution_1 = np.random.randn(1000,2) + np.array([-4,-4])
-distribution_2 = np.random.randn(1000,2) + np.array([-4, 4])
-distribution_3 = np.random.randn(1000,2) + np.array([4, -4])
-distribution_4 = np.random.randn(1000,2) + np.array([4,4])
-
-anomalies_1 = np.array([[0,0], [-0.5,0.3], [0.4,-0.7]], dtype=np.float32)
-
-
-sns.scatterplot(x=distribution_1[:,0], y=distribution_1[:,1], color="blue", alpha=0.2)
-sns.scatterplot(x=distribution_2[:,0], y=distribution_2[:,1], color="blue", alpha=0.2)
-sns.scatterplot(x=distribution_3[:,0], y=distribution_3[:,1], color="blue", alpha=0.2)
-sns.scatterplot(x=distribution_4[:,0], y=distribution_4[:,1], color="blue", alpha=0.2)
-sns.scatterplot(x=anomalies_1[:,0], y=anomalies_1[:,1], marker="X", color="red", s=100)
-plt.xlim(-8, 8)
-plt.ylim(-8, 8)
-#plt.legend(loc='upper left')
-
-plt.xlabel("X$_1$")
-plt.ylabel("X$_2$")
+# Show the plot
 
 
-#plt.title("Enclosed Anomalies")
+fig.savefig("figures/enclosed-peripheral_point_example.pdf", format="pdf")
 
-plt.savefig("figures/enclosed_point_example.pdf", format="pdf")
+plt.show()
 #%% local_outlier_plot
 
+# Local outlier plot
 local_outlier_plot = plt.figure()
 
-distribution_1 = np.random.randn(1000,2) + np.array([1,1])
-distribution_2 = np.random.randn(1000,2)/5 + np.array([7,7])
+distribution_1 = np.random.randn(1000, 2) + np.array([1, 1])
+distribution_2 = np.random.randn(1000, 2) / 5 + np.array([7, 7])
+
+anomalies = np.array([[6.2, 6.5], [7.2, 8], [7.9, 6.3]])
+
+fig, axes = plt.subplots(1, 2, figsize=(18, 6))
+
+sns.scatterplot(x=distribution_1[:, 0], y=distribution_1[:, 1], color="blue", alpha=0.2, ax=axes[0])
+sns.scatterplot(x=distribution_2[:, 0], y=distribution_2[:, 1], color="blue", alpha=0.2, ax=axes[0])
+sns.scatterplot(x=anomalies[:, 0], y=anomalies[:, 1], marker="X", color="red", s=100, ax=axes[0])
+axes[0].set_xlim(-6, 10)
+axes[0].set_ylim(-6, 10)
+axes[0].set_xlabel("X$_1$")
+axes[0].set_ylabel("X$_2$")
+axes[0].set_title("Local Density Anomalies", fontsize=20)
+
+# Global outlier plot
+anomalies = np.array([[8, 0], [7.5, 1]])
+
+sns.scatterplot(x=distribution_1[:, 0], y=distribution_1[:, 1], color="blue", alpha=0.2, ax=axes[1])
+sns.scatterplot(x=distribution_2[:, 0], y=distribution_2[:, 1], color="blue", alpha=0.2, ax=axes[1])
+sns.scatterplot(x=anomalies[:, 0], y=anomalies[:, 1], marker="X", color="red", s=100, ax=axes[1])
+axes[1].set_xlim(-6, 10)
+axes[1].set_ylim(-6, 10)
+axes[1].set_xlabel("X$_1$")
+axes[1].set_ylabel("X$_2$")
+axes[1].set_title("Global Density Anomalies", fontsize=20)
+
+# Adjust layout to prevent clipping of titles
+plt.tight_layout()
+
+# Show the plot
 
 
-anomalies = np.array([[6.2,6.5], [7.2, 8], [7.9,6.3]])
+fig.savefig("figures/global-local_outlier_example.pdf", format="pdf")
 
-plt.figure()
-sns.scatterplot(x=distribution_1[:,0], y=distribution_1[:,1], color="blue", alpha=0.2)
-sns.scatterplot(x=distribution_2[:,0], y=distribution_2[:,1], color="blue", alpha=0.2)
-
-sns.scatterplot(x=anomalies[:,0], y=anomalies[:,1], marker="X", color="red", s=100)
-plt.xlim(-6, 10)
-plt.ylim(-6, 10)
-#plt.legend(loc='upper left')
-
-plt.xlabel("X$_1$")
-plt.ylabel("X$_2$")
-
-
-#plt.title("Local Density Anomalies")
-
-
-plt.savefig("figures/local_outlier_example.pdf", format="pdf")
-
-#%% Global outlier_plot
-
-anomalies = np.array([[8,0], [7.5,1]])
-
-plt.figure()
-sns.scatterplot(x=distribution_1[:,0], y=distribution_1[:,1], color="blue", alpha=0.2)
-sns.scatterplot(x=distribution_2[:,0], y=distribution_2[:,1], color="blue", alpha=0.2)
-
-sns.scatterplot(x=anomalies[:,0], y=anomalies[:,1], marker="X", color="red", s=100)
-plt.xlim(-6, 10)
-plt.ylim(-6, 10)
-
-plt.xlabel("X$_1$")
-plt.ylabel("X$_2$")
-
-
-#plt.title("Global Density Anomalies")
-
-
-plt.savefig("figures/global_outlier_example.pdf", format="pdf")
+plt.show()
 #%% clustered outliers
 
+# Clustered outliers plot
 local_outlier_plot = plt.figure()
 
-distribution_1 = np.random.randn(1000,2) + np.array([1,1])
-anomalies_2 = np.random.randn(10,2)/5 + np.array([7,7])
+distribution_1 = np.random.randn(1000, 2) + np.array([1, 1])
+anomalies_2 = np.random.randn(10, 2) / 5 + np.array([7, 7])
 
-plt.figure()
-sns.scatterplot(x=distribution_1[:,0], y=distribution_1[:,1], color="blue", alpha=0.2)
+fig, axes = plt.subplots(1, 2, figsize=(18, 6))
 
-sns.scatterplot(x=anomalies_2[:,0], y=anomalies_2[:,1], marker="X", color="red", s=100)
-plt.xlim(-6, 10)
-plt.ylim(-6, 10)
-#plt.legend(loc='upper left')
+sns.scatterplot(x=distribution_1[:, 0], y=distribution_1[:, 1], color="blue", alpha=0.2, ax=axes[0])
+sns.scatterplot(x=anomalies_2[:, 0], y=anomalies_2[:, 1], marker="X", color="red", s=100, ax=axes[0])
+axes[0].set_xlim(-6, 10)
+axes[0].set_ylim(-6, 10)
+axes[0].set_xlabel("X$_1$")
+axes[0].set_ylabel("X$_2$")
+axes[0].set_title("Clustered Anomalies", fontsize=20)
 
-plt.xlabel("X$_1$")
-plt.ylabel("X$_2$")
+# Isolated outliers plot
+anomalies = np.array([[7, 7], [-4, -4], [-4, 6]])
 
+sns.scatterplot(x=distribution_1[:, 0], y=distribution_1[:, 1], color="blue", alpha=0.2, ax=axes[1])
+sns.scatterplot(x=anomalies[:, 0], y=anomalies[:, 1], marker="X", color="red", s=100, ax=axes[1])
+axes[1].set_xlim(-6, 10)
+axes[1].set_ylim(-6, 10)
+axes[1].set_xlabel("X$_1$")
+axes[1].set_ylabel("X$_2$")
+axes[1].set_title("Isolated Anomalies", fontsize=20)
 
-#plt.title("Clustered Anomalies")
+# Adjust layout to prevent clipping of titles
+plt.tight_layout()
 
-plt.savefig("figures/clustered_outlier_example.pdf", format="pdf")
-
-#%% clustered outliers
-
-local_outlier_plot = plt.figure()
-
-anomalies = np.array([[7,7], [-4,-4], [-4,6]])
-
-plt.figure()
-sns.scatterplot(x=distribution_1[:,0], y=distribution_1[:,1], color="blue", alpha=0.2)
-
-sns.scatterplot(x=anomalies[:,0], y=anomalies[:,1], marker="X", color="red", s=100)
-plt.xlim(-6, 10)
-plt.ylim(-6, 10)
-#plt.legend(loc='upper left')
-
-plt.xlabel("X$_1$")
-plt.ylabel("X$_2$")
+# Show the plot
 
 
-#plt.title("Isolated Anomalies")
-
-plt.savefig("figures/isolated_outlier_example.pdf", format="pdf")
-
+fig.savefig("figures/isolated-clustered_outlier_example.pdf", format="pdf")
+plt.show()
 #%% univariate outliers
 
-distribution_1 = np.random.multivariate_normal([0,0], [[3,0,],[0,1]], 1000)
+# Univariate outliers plot
+distribution_1 = np.random.multivariate_normal([0, 0], [[3, 0], [0, 1]], 1000)
+anomalies = np.array([[0, 6], [8, 0]])
 
-anomalies = np.array([[0,6],[8,0]])
+fig, axes = plt.subplots(1, 2, figsize=(18, 6))
+
+sns.scatterplot(x=distribution_1[:, 0], y=distribution_1[:, 1], color="blue", alpha=0.2, ax=axes[0])
+sns.scatterplot(x=anomalies[:, 0], y=anomalies[:, 1], marker="X", color="red", s=100, ax=axes[0])
+axes[0].set_xlim(-9, 9)
+axes[0].set_ylim(-4, 7)
+axes[0].set_xlabel("X$_1$")
+axes[0].set_ylabel("X$_2$")
+axes[0].set_title("Univariate Anomalies", fontsize=20)
+
+# Multivariate outliers plot
+distribution_1 = np.random.multivariate_normal([0, 0], [[0.5, 0], [4, 4]], 1000)
+anomalies = np.array([[-3, 3], [4, -2]])
+
+sns.scatterplot(x=distribution_1[:, 0], y=distribution_1[:, 1], color="blue", alpha=0.2, ax=axes[1])
+sns.scatterplot(x=anomalies[:, 0], y=anomalies[:, 1], marker="X", color="red", s=100, ax=axes[1])
+axes[1].set_xlim(-7, 7)
+axes[1].set_ylim(-7, 7)
+axes[1].set_xlabel("X$_1$")
+axes[1].set_ylabel("X$_2$")
+axes[1].set_title("Multivariate Anomalies", fontsize=20)
+
+# Adjust layout to prevent clipping of titles
+plt.tight_layout()
+
+# Show the plot
 
 
-plt.figure()
-sns.scatterplot(x=distribution_1[:,0], y=distribution_1[:,1], color="blue", alpha=0.2)
-
-sns.scatterplot(x=anomalies[:,0], y=anomalies[:,1], marker="X", color="red", s=100)
-plt.xlim(-9,9)
-plt.ylim(-4,7)
-#plt.legend(loc='upper left')
-
-plt.xlabel("X$_1$")
-plt.ylabel("X$_2$")
-
-#plt.title("Univariate Anomalies")
-
-plt.savefig("figures/univariate_outlier_example.pdf", format="pdf")
-
-#%% multivariate outliers
-
-distribution_1 = np.random.multivariate_normal([0,0], [[0.5,0],[4,4]], 1000)
-
-anomalies = np.array([[-3,3],[4,-2]])
-
-
-plt.figure()
-sns.scatterplot(x=distribution_1[:,0], y=distribution_1[:,1], color="blue", alpha=0.2)
-
-sns.scatterplot(x=anomalies[:,0], y=anomalies[:,1], marker="X", color="red", s=100)
-plt.xlim(-7,7)
-plt.ylim(-7,7)
-#plt.legend(loc='upper left')
-
-plt.xlabel("X$_1$")
-plt.ylabel("X$_2$")
-
-#plt.title("Multivariate Anomalies")
-
-plt.savefig("figures/multivariate_outlier_example.pdf", format="pdf")
-
+fig.savefig("figures/multivariate-univariate_outlier_example.pdf", format="pdf")
+plt.show()
